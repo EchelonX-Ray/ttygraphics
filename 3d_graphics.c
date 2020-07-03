0e4ad12fb0e57287416161fed717489e661a85dc
Michael T. Kloos <michael@michaelkloos.com>
2020-07-02 21:52:27 -0400

    Dev

--- 3d_graphics.c
+++ 3d_graphics.c
@@ -36,7 +36,7 @@ struct slope {
 	float offset;
 };
 
-unsigned int is_in_fov(struct camera* c, struct matrix* m);
+unsigned int is_in_fov(struct camera* c, struct matrix* m, struct matrix* buffer);
 void translate_rotation(struct matrix* point, struct matrix* rotation_point, struct matrix* rotation_delta, struct matrix* buffer);
 float points_to_angle_2d(float x1, float y1, float x2, float y2);
 void angle_to_points_2d(float angle, float magnitude, float x1, float y1, float* x2, float* y2);
@@ -153,8 +153,9 @@ signed int main(signed int argc, char* argv[], char* envp[]) {
 	void* ptr = 0;
 	unsigned int screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
 	
+	errno = 0;
 	ptr = mmap(ptr, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-	if (ptr == MAP_FAILED || ptr == 0) {
+	if (ptr == MAP_FAILED || ptr == 0 || errno != 0) {
 		printf("Error: mmap() Failed!  Could not create virtual address space mapping!\n");
 		printf("errno: %s\n", strerror(errno));
 		return 7;
@@ -244,9 +245,19 @@ signed int main(signed int argc, char* argv[], char* envp[]) {
 	box.lines[11].p0.x = -1.0;
 	box.lines[11].p0.y = -1.0;
 	box.lines[11].p0.z = -1.0;
-	box.lines[11].p1.x = -50.0;
+	box.lines[11].p1.x = -1.0;
 	box.lines[11].p1.y = -1.0;
 	box.lines[11].p1.z = +1.0;
+	
+	uint32_t pt_colors[8];
+	pt_colors[ 0] = 0x00FFFFFF;
+	pt_colors[ 1] = 0x0000FFFF;
+	pt_colors[ 2] = 0x00FF00FF;
+	pt_colors[ 3] = 0x00FFFF00;
+	pt_colors[ 4] = 0x000000FF;
+	pt_colors[ 5] = 0x0000FF00;
+	pt_colors[ 6] = 0x00FF0000;
+	pt_colors[ 7] = 0x007F7F7F;
 	
 	cam.location.x =   0.0;
 	cam.location.y =   0.0;
@@ -255,84 +266,28 @@ signed int main(signed int argc, char* argv[], char* envp[]) {
 	cam.looking_at.y =   0.0;
 	cam.looking_at.z =   0.0;
 	
-	/*
-	struct matrix cam_vect_min;
-	struct matrix cam_vect_max;
-	
-	cam_vect_min.z = 1.0;
-	*/
-	
 	float max_fov_degrees = 90.0;
-	unsigned int width = vinfo.xres;
-	unsigned int height = vinfo.yres;
 	
-	if (width > height) {
+	if (vinfo.xres > vinfo.yres) {
 		cam.h_fov = max_fov_degrees;
-		cam.v_fov = (max_fov_degrees * height) / width;
+		cam.v_fov = (max_fov_degrees * vinfo.yres) / vinfo.xres;
 	} else {
 		cam.v_fov = max_fov_degrees;
-		cam.h_fov = (max_fov_degrees * width) / height;
+		cam.h_fov = (max_fov_degrees * vinfo.xres) / vinfo.yres;
 	}
 	
-	// Start: Debuging code
-	/*
-	printf("HFOV: %f, VFOV: %f\n\n", cam.h_fov, cam.v_fov);
-	
-	cam.looking_at.x =   0.0;
-	cam.looking_at.y =   0.0;
-	cam.looking_at.z =  -2.0;
-	is_in_fov(&cam, &box.lines[ 0].p0);
-	
-	cam.looking_at.x =   0.0;
-	cam.looking_at.y =  -0.1;
-	cam.looking_at.z =  -2.0;
-	is_in_fov(&cam, &box.lines[ 0].p0);
-	
-	cam.looking_at.x =  -0.1;
-	cam.looking_at.y =   0.0;
-	cam.looking_at.z =  -0.0;
-	is_in_fov(&cam, &box.lines[ 0].p0);
-	
-	cam.looking_at.x =   0.0;
-	cam.looking_at.y =   1.0;
-	cam.looking_at.z =  -1.0;
-	is_in_fov(&cam, &box.lines[ 0].p0);
-	
-	cam.looking_at.x =   1.0;
-	cam.looking_at.y =   1.0;
-	cam.looking_at.z =  -2.0;
-	is_in_fov(&cam, &box.lines[ 0].p0);
-	goto end;
-	*/
-	// End: Debuging code
-	
-	/*
-	cam_vect_max.z = cam_vect_min.z;
-	
-	unsigned int width = vinfo.xres;
-	unsigned int height = vinfo.yres;
-	if (height < width) {
-		cam_vect_min.x = -tanf(max_fov_degrees / 2.0) * cam_vect_min.z;
-		cam_vect_min.y = +tanf((max_fov_degrees * (height / width)) / 2.0) * cam_vect_min.z;
-		cam_vect_max.x = +tanf(max_fov_degrees / 2.0) * cam_vect_max.z;
-		cam_vect_max.y = -tanf((max_fov_degrees * (height / width)) / 2.0) * cam_vect_max.z;
-	} else {
-		cam_vect_min.y = +tanf(max_fov_degrees / 2.0) * cam_vect_min.z;
-		cam_vect_min.x = -tanf((max_fov_degrees * (width / height)) / 2.0) * cam_vect_min.z;
-		cam_vect_max.y = -tanf(max_fov_degrees / 2.0) * cam_vect_max.z;
-		cam_vect_max.x = +tanf((max_fov_degrees * (width / height)) / 2.0) * cam_vect_max.z;
-	}
-	*/
-	
-	uint32_t color = 0x00FFFFFF;
+	struct matrix buffer;
+	uint32_t color = 0;
 	unsigned int i = 0;
-	unsigned int a = 0;
+	//unsigned int a = 0;
 	unsigned int x = 0;
 	unsigned int y = 0;
-	while (i < 12) {
+	while (i < 8) {
 		//printf("\n------------------------------\n\n");
-		printf("ia: %d\n", i);
-		if (is_in_fov(&cam, &(box.lines[i].p0))) {
+		//printf("ia: %d\n", i);
+		if (is_in_fov(&cam, &(box.lines[i].p0), &buffer)) {
+			/*
+			color = 0x00FFFFFF;
 			x = 0;
 			while (x < 20) {
 				y = 0;
@@ -342,16 +297,26 @@ signed int main(signed int argc, char* argv[], char* envp[]) {
 				}
 				x++;
 			}
-			printf("ia: Success\n", i);
-		} else {
-			printf("ia: Failed\n", i);
-		}
-		printf("\n");
-		a += 40;
+			*/
+			
+			//color = 0x0000FF00;
+			color = pt_colors[i];
+			x = buffer.x / cam.h_fov * vinfo.xres;
+			y = buffer.y / cam.v_fov * vinfo.yres;
+			mcolor[y * vinfo.xres + x] = color;
+			
+			//printf("ia: Success\n", i);
+		}// else {
+			//printf("ia: Failed\n", i);
+		//}
+		//printf("\n");
+		//a += 40;
 		
 		//printf("\n------------------------------\n\n");
-		printf("ib: %d\n", i);
-		if (is_in_fov(&cam, &(box.lines[i].p1))) {
+		//printf("ib: %d\n", i);
+		/*
+		if (is_in_fov(&cam, &(box.lines[i].p1), &buffer)) {
+			color = 0x00FFFFFF;
 			x = 0;
 			while (x < 20) {
 				y = 0;
@@ -361,12 +326,19 @@ signed int main(signed int argc, char* argv[], char* envp[]) {
 				}
 				x++;
 			}
-			printf("ib: Success\n", i);
-		} else {
-			printf("ib: Failed\n", i);
-		}
-		printf("\n");
-		a += 40;
+			
+			color = 0x0000FF00;
+			x = buffer.x * vinfo.xres / cam.h_fov;
+			y = buffer.y * vinfo.yres / cam.v_fov;
+			mcolor[y * vinfo.xres + x] = color;
+			
+			//printf("ib: Success\n", i);
+		}// else {
+			//printf("ib: Failed\n", i);
+		//}
+		*/
+		//printf("\n");
+		//a += 40;
 		
 		i++;
 	}
@@ -382,97 +354,25 @@ signed int main(signed int argc, char* argv[], char* envp[]) {
 	return 0;
 }
 
-unsigned int is_in_fov(struct camera* c, struct matrix* m) {
-	//tanf(c.h_fov / 2.0) * cam_vect_min.z;
-	
-	/*
-	struct matrix low_angle;
-	struct matrix high_angle;
-	
-	// TODO: calculate low_angle and high_angle
-	float x_angle = 0.0;
-	float y_angle = 0.0;
-	//float z_angle = 0.0;
-	//c->h_fov;
-	//c->v_fov;
-	float delta_x;
-	float delta_y;
-	float delta_z;
-	//slope_x = c->location.x c->looking_at.x c->location.y c->looking_at.y
-	delta_x = c->looking_at.x - c->location.x;
-	delta_y = c->looking_at.y - c->location.y;
-	delta_z = c->looking_at.z - c->location.z;
-	if (delta_y == 0) {
-		x_angle = 0.0;
-	} else {
-		x_angle = atanf(delta_y / sqrtf((delta_x * delta_x) + (delta_z * delta_z)));
-	}
-	if (delta_x == 0) {
-		y_angle = 0.0;
-	} else {
-		y_angle = atanf(delta_x / delta_z);
-	}
-	if (delta_z < 0) {
-		//x_angle = -x_angle;
-		y_angle += M_PI;
-		if (y_angle > 180.0) {
-			y_angle -= 360.0;
-		}
-	}
-	
-	printf("x_angle: %f\n", x_angle * 180.0 / M_PI);
-	printf("y_angle: %f\n", y_angle * 180.0 / M_PI);
-	printf("\n");
-	*/
-	
-	/*
-	float l_xangle;
-	float h_xangle;
-	float l_yangle;
-	float h_yangle;
-	l_yangle = 0.0;
-	h_yangle = 0.0;
-	l_xangle = x_angle - ((c->v_fov * M_PI) / 360.0);
-	h_xangle = x_angle + ((c->v_fov * M_PI) / 360.0);
-	if (l_xangle > M_PI_2) {
-		l_xangle = M_PI - l_xangle;
-		l_yangle += M_PI;
-	} else if (l_xangle < -90.0) {
-		l_xangle = M_PI + l_xangle;
-		l_yangle += M_PI;
-	}
-	if (h_xangle > M_PI_2) {
-		h_xangle = M_PI - h_xangle;
-		h_yangle += M_PI;
-	} else if (h_xangle < -90.0) {
-		h_xangle = M_PI + h_xangle;
-		h_yangle += M_PI;
-	}
-	l_yangle += y_angle - ((c->h_fov * M_PI) / 360.0);
-	h_yangle += y_angle + ((c->h_fov * M_PI) / 360.0);
-	low_angle.z = c->location.z + cosf(l_yangle);
-	low_angle.x = c->location.x + sinf(l_yangle);
-	low_angle.y = c->location.y + sinf(l_xangle);
-	high_angle.z = c->location.z + cosf(h_yangle);
-	high_angle.x = c->location.x + sinf(h_yangle);
-	high_angle.y = c->location.y + sinf(h_xangle);
-	
-	printf("l_xangle = %f, l_yangle = %f\n", l_xangle * 180 / M_PI, l_yangle * 180 / M_PI);
-	printf("h_xangle = %f, h_yangle = %f\n", h_xangle * 180 / M_PI, h_yangle * 180 / M_PI);
-	printf("\n");
-	printf("low_angle: x = %f, y = %f, z = %f\n", low_angle.x, low_angle.y, low_angle.z);
-	printf("high_angle: x = %f, y = %f, z = %f\n", high_angle.x, high_angle.y, high_angle.z);
-	printf("\n");
-	*/
-	
-	
-	struct matrix buffer;
+unsigned int is_in_fov(struct camera* c, struct matrix* m, struct matrix *buffer) {
+	// Translate point(struct matrix* m) to the camera angles
+	struct matrix buffer2;
 	struct matrix rotation_delta;
 	rotation_delta.z = 0.0;
 	rotation_delta.y = points_to_angle_2d(c->location.x, c->location.z, c->looking_at.x, c->looking_at.z);
 	rotation_delta.x = points_to_angle_2d(c->location.y, c->location.z, c->looking_at.y, c->looking_at.z);
-	translate_rotation(m, &(c->location), &rotation_delta, &buffer);
+	translate_rotation(m, &(c->location), &rotation_delta, &buffer2);
 	
+	// Find the max and min slopes and offsets, per the equation [y = mx + b], for the camera's FOV and location.
+	// The camera is assumed to be pointing straight forward with no angles since the point is translated to 
+	// match it above instead.  4 relationships are computed: A min and max corresponding to the camera's FOV 
+	// along the x/z and y/z axis.
+	
+	// Variable Nameing Convention:
+	//   Minimums are prefixed with l_ and Maximums are prefixed with h_
+	//   Association of the Z-Axis as the x varable in the equation [y = mx + b] is assumed.
+	//   The value with an association to the y varable in the equation [y = mx + b], is specifed by the value after l_ or h_.
+	//   Whether the value is associated with m or b is indicated by weather it is suffixed with a slope or offset, respectively.
 	float h_xslope;
 	float h_yslope;
 	float l_xslope;
@@ -490,243 +390,28 @@ unsigned int is_in_fov(struct camera* c, struct matrix* m) {
 	l_xoffset = c->location.y - (l_yslope * c->location.z);
 	l_yoffset = c->location.x - (l_xslope * c->location.z);
 	
-	//printf("m->x: %f, m->y: %f, m->z: %f\n", m->x, m->y, m->z);
-	//printf("b->x: %f, b->y: %f, b->z: %f\n", buffer.x, buffer.y, buffer.z);
-	
-	if (buffer.x > (h_xslope * buffer.z) + h_xoffset) {
+	// Plug the values into the 4 computed equations and determine whether the point is outside the camera's FOV.
+	// If it is, return 0.
+	if (buffer2.x > (h_xslope * buffer2.z) + h_xoffset) {
 		return 0;
 	}
-	if (buffer.x < (l_xslope * buffer.z) + l_xoffset) {
+	if (buffer2.x < (l_xslope * buffer2.z) + l_xoffset) {
 		return 0;
 	}
-	if (buffer.y > (h_yslope * buffer.z) + h_yoffset) {
+	if (buffer2.y > (h_yslope * buffer2.z) + h_yoffset) {
 		return 0;
 	}
-	if (buffer.y < (l_yslope * buffer.z) + l_yoffset) {
+	if (buffer2.y < (l_yslope * buffer2.z) + l_yoffset) {
 		return 0;
 	}
 	
-	/*
-	// Calculate x-y y-z x-z for low_angle
-	//struct slope la_x_y;
-	struct slope la_y_z;
-	struct slope la_x_z;
-	// Calc x-y and x-z
-	if (c->location.x == low_angle.x) {
-		//la_x_y.type = 1;
-		la_x_z.type = 1;
-		//la_x_y.offset = c->location.x;
-		la_x_z.offset = c->location.x;
-	} else {
-		//la_x_y.type = 0;
-		la_x_z.type = 0;
-		//la_x_y.slope = (c->location.y - low_angle.y) / (c->location.x - low_angle.x);
-		la_x_z.slope = (c->location.z - low_angle.z) / (c->location.x - low_angle.x);
-		//la_x_y.offset = c->location.y - (la_x_y.slope * c->location.x);
-		la_x_z.offset = c->location.z - (la_x_z.slope * c->location.x);
-	}
-	// Calc y-z
-	if (c->location.y == low_angle.y) {
-		la_y_z.type = 1;
-		la_y_z.offset = c->location.y;
-	} else {
-		la_y_z.type = 0;
-		la_y_z.slope = (c->location.z - low_angle.z) / (c->location.y - low_angle.y);
-		la_y_z.offset = c->location.z - (la_y_z.slope * c->location.y);
-	}
-	
-	// Calculate x-y y-z x-z for high_angle
-	//struct slope ha_x_y;
-	struct slope ha_y_z;
-	struct slope ha_x_z;
-	// Calc x-y and x-z
-	if (c->location.x == high_angle.x) {
-		//ha_x_y.type = 1;
-		ha_x_z.type = 1;
-		//ha_x_y.offset = c->location.x;
-		ha_x_z.offset = c->location.x;
-	} else {
-		//ha_x_y.type = 0;
-		ha_x_z.type = 0;
-		//ha_x_y.slope = (c->location.y - high_angle.y) / (c->location.x - high_angle.x);
-		ha_x_z.slope = (c->location.z - high_angle.z) / (c->location.x - high_angle.x);
-		//ha_x_y.offset = c->location.y - (ha_x_y.slope * c->location.x);
-		ha_x_z.offset = c->location.z - (ha_x_z.slope * c->location.x);
-	}
-	// Calc y-z
-	if (c->location.y == high_angle.y) {
-		ha_y_z.type = 1;
-		ha_y_z.offset = c->location.y;
-	} else {
-		ha_y_z.type = 0;
-		ha_y_z.slope = (c->location.z - high_angle.z) / (c->location.y - high_angle.y);
-		ha_y_z.offset = c->location.z - (ha_y_z.slope * c->location.y);
-	}
-	
-	if (ha_y_z) {
-	}
-	
-	// Determine whether the calculation should be less-than or greater-than 
-	// per each slope based on the point the camera is pointing at.
-	// Do the low_angle
-	if (la_x_y.type == 0) {
-		if (c->looking_at.y < (la_x_y.slope * c->looking_at.x) + la_x_y.offset) {
-			if (m->y > (la_x_y.slope * m->x) + la_x_y.offset) {
-				printf("TraceA\n");
-				return 0;
-			}
-		} else {
-			if (m->y < (la_x_y.slope * m->x) + la_x_y.offset) {
-				printf("[DEBUG] m->y: %f\n", m->y);
-				printf("[DEBUG] m->x: %f\n", m->x);
-				printf("[DEBUG] m->z: %f\n", m->z);
-				printf("[DEBUG] la_x_y.slope: %f\n", la_x_y.slope);
-				printf("[DEBUG] la_x_y.offset: %f\n", la_x_y.offset);
-				printf("TraceB\n");
-				return 0;
-			}
-		}
-	} else {
-		if (c->looking_at.x < la_x_y.offset) {
-			if (m->x > la_x_y.offset) {
-				printf("TraceC\n");
-				return 0;
-			}
-		} else if (c->looking_at.x > la_x_y.offset) {
-			if (m->x < la_x_y.offset) {
-				printf("TraceD\n");
-				return 0;
-			}
-		}
-	}
-	if (la_y_z.type == 0) {
-		if (c->looking_at.z < (la_y_z.slope * c->looking_at.y) + la_y_z.offset) {
-			if (m->z > (la_y_z.slope * m->y) + la_y_z.offset) {
-				printf("TraceE\n");
-				return 0;
-			}
-		} else if (c->looking_at.z > (la_y_z.slope * c->looking_at.y) + la_y_z.offset) {
-			if (m->z < (la_y_z.slope * m->y) + la_y_z.offset) {
-				printf("TraceF\n");
-				return 0;
-			}
-		}
-	} else {
-		if (c->looking_at.y < la_y_z.offset) {
-			if (m->y > la_y_z.offset) {
-				printf("TraceG\n");
-				return 0;
-			}
-		} else if (c->looking_at.y > la_y_z.offset) {
-			if (m->y < la_y_z.offset) {
-				printf("TraceH\n");
-				return 0;
-			}
-		}
-	}
-	if (la_x_z.type == 0) {
-		if (c->looking_at.z < (la_x_z.slope * c->looking_at.x) + la_x_z.offset) {
-			if (m->z > (la_x_z.slope * m->x) + la_x_z.offset) {
-				printf("TraceI\n");
-				return 0;
-			}
-		} else if (c->looking_at.z > (la_x_z.slope * c->looking_at.x) + la_x_z.offset) {
-			if (m->z < (la_x_z.slope * m->x) + la_x_z.offset) {
-				printf("TraceJ\n");
-				return 0;
-			}
-		}
-	} else {
-		if (c->looking_at.x < la_x_z.offset) {
-			if (m->x > la_x_z.offset) {
-				printf("TraceK\n");
-				return 0;
-			}
-		} else if (c->looking_at.x > la_x_z.offset) {
-			if (m->x < la_x_z.offset) {
-				printf("TraceL\n");
-				return 0;
-			}
-		}
-	}
-	// Do the high_angle
-	if (ha_x_y.type == 0) {
-		if (c->looking_at.y < (ha_x_y.slope * c->looking_at.x) + ha_x_y.offset) {
-			if (m->y > (ha_x_y.slope * m->x) + ha_x_y.offset) {
-				printf("TraceM\n");
-				return 0;
-			}
-		} else if (c->looking_at.y > (ha_x_y.slope * c->looking_at.x) + ha_x_y.offset) {
-			if (m->y < (ha_x_y.slope * m->x) + ha_x_y.offset) {
-				printf("TraceN\n");
-				return 0;
-			}
-		}
-	} else {
-		if (c->looking_at.x < ha_x_y.offset) {
-			if (m->x > ha_x_y.offset) {
-				printf("TraceO\n");
-				return 0;
-			}
-		} else if ((c->looking_at.x > ha_x_y.offset)) {
-			if (m->x < ha_x_y.offset) {
-				printf("TraceP\n");
-				return 0;
-			}
-		}
-	}
-	if (ha_y_z.type == 0) {
-		if (c->looking_at.z < (ha_y_z.slope * c->looking_at.y) + ha_y_z.offset) {
-			if (m->z > (ha_y_z.slope * m->y) + ha_y_z.offset) {
-				printf("TraceQ\n");
-				return 0;
-			}
-		} else if (c->looking_at.z > (ha_y_z.slope * c->looking_at.y) + ha_y_z.offset) {
-			if (m->z < (ha_y_z.slope * m->y) + ha_y_z.offset) {
-				printf("TraceR\n");
-				return 0;
-			}
-		}
-	} else {
-		if (c->looking_at.y < ha_y_z.offset) {
-			if (m->y > ha_y_z.offset) {
-				printf("TraceS\n");
-				return 0;
-			}
-		} else if (c->looking_at.y > ha_y_z.offset) {
-			if (m->y < ha_y_z.offset) {
-				printf("TraceT\n");
-				return 0;
-			}
-		}
-	}
-	if (ha_x_z.type == 0) {
-		if (c->looking_at.z < (ha_x_z.slope * c->looking_at.x) + ha_x_z.offset) {
-			if (m->z > (ha_x_z.slope * m->x) + ha_x_z.offset) {
-				printf("TraceU\n");
-				return 0;
-			}
-		} else if (c->looking_at.z > (ha_x_z.slope * c->looking_at.x) + ha_x_z.offset) {
-			if (m->z < (ha_x_z.slope * m->x) + ha_x_z.offset) {
-				printf("TraceV\n");
-				return 0;
-			}
-		}
-	} else {
-		if (c->looking_at.x < ha_x_z.offset) {
-			if (m->x > ha_x_z.offset) {
-				printf("TraceW\n");
-				return 0;
-			}
-		} else if (c->looking_at.x > ha_x_z.offset) {
-			if (m->x < ha_x_z.offset) {
-				printf("TraceX\n");
-				return 0;
-			}
-		}
-	}
-	*/
-	
+	// If we have made it this far, we are in the camera's FOV.  So we should:
+	//   -Store in the return buffer, the proportion of the location of the point, to the camera's FOV, for the x and y axis.
+	//   --Translate as necessary such that the minimum X and Y FOVs start at 0 and not at -0.5 * (FOV angle).
+	//   -Return 1.
+	buffer->x = points_to_angle_2d(c->location.x, c->location.z, buffer2.x, buffer2.z) * 180 / M_PI + (c->h_fov / 2);
+	buffer->y = points_to_angle_2d(c->location.y, c->location.z, buffer2.y, buffer2.z) * 180 / M_PI + (c->v_fov / 2);
+	buffer->z = 0.0; // This value is not used.
 	return 1;
 }
 void translate_rotation(struct matrix* target_point, struct matrix* rotation_point, struct matrix* rotation_delta, struct matrix* buffer) {
